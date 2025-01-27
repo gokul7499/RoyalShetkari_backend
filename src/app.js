689a68d935fbe5ecd7156eadb8b5bf5f617016c0
gudaghers <gudaghers@gmail.com>
2025-01-27 10:56:56 +0530

    1 change

--- src/app.js
+++ src/app.js
@@ -1,27 +1,29 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const contactRoutes = require('./routes/contactRoutes');
-const visitorRoutes = require('./routes/Visitor'); 
-const emailRoutes = require('./routes/email');
-require('dotenv').config(); 
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const contactRoutes = require("./routes/contactRoutes");
+const visitorRoutes = require("./routes/Visitor");
+const emailRoutes = require("./routes/email");
+require("dotenv").config();
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/api', contactRoutes);
-app.use('/api', visitorRoutes); 
-app.use('/api',emailRoutes)
+app.use("/api", contactRoutes);
+app.use("/api", visitorRoutes);
+app.use("/api", emailRoutes);
 
-console.log('gokul');
+console.log("gokul");
 // connect with mongodb
 mongoose
-  .connect(process.env.DBHOST, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log('MongoDB connection error:', err));
+  .connect(process.env.DBHOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err));
 
 module.exports = app;
-
